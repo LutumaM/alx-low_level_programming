86c003f91b9545db10b05a5d7f045bd5390372e2
LutumaM <lutumbamiriam@gmail.com>
2022-03-24 15:25:48 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,24 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = "\t\n,;.!?\"(){}";
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
-	while (*(s + i))
+	while (s[a])
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i -1))
-						*(s + i) -= 'a' - 'A';
-				}
-			}
-		}
-		i++;
-	}
+		i = 0;
 
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+
+		a++;
+	}
+	
 	return (s);
 }
