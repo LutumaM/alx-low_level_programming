04762b03e233d17327a75101cd1ff82efaef7437
LutumaM <lutumbamiriam@gmail.com>
2022-03-28 15:53:57 -0700

    Advanced Task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Set a pointer to char
+ * @s: Pointer to pointer
+ * @to: Pointet to char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
