cf1928aab6844b5d094e0b7fed8e33d1733f9f23
LutumaM <lutumbamiriam@gmail.com>
2022-04-01 15:35:50 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,18 +10,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
+	int x, i, j;
 
-	if (argc == 3)
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("ERROR\n");
 		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		x = i * j;
+		printf("%d\n", x);
 	}
 	
 	return (0);
