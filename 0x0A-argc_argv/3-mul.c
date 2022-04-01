492f836d25ab1a7f70a118f5c4ee50e8c17bc102
LutumaM <lutumbamiriam@gmail.com>
2022-04-01 15:03:37 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiples two arguments
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Alwzys zero
+ */
+int main(int argc, char *argv[])
+{
+	int x, i, j;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		x = i * j;
+		printf("%d\n", x);
+	}
+
+	return (0);
+}
