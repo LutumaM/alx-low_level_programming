3dbd107ee3c856a4bcf029e5f72bf48e1410316f
LutumaM <lutumbamiriam@gmail.com>
2022-04-08 15:17:43 -0700

    Task 1 revised

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,21 +16,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[i])
+	{
 		i++;
+	}
 
 	while (s2[k])
+	{
 		k++;
+	}
 
 	if (n >= k)
+	{
 		l = i + k;
+	}
 	else
+	{
 		l = i + n;
+	}
 
 	str = malloc(sizeof(char) * l + 1);
+
 	if (str == NULL)
 		return (NULL);
 
@@ -48,5 +58,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		j++;
 	}
 	str[j] = '\0';
+
 	return (str);
 }
